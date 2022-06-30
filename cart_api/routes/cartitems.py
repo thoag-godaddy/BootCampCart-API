7c47d3b9604681841fd05e9a81c7caefeb7408f6
Tylar Hoag <thoag@godaddy.com>
2022-06-30 16:39:28 -0700

    exercise 3 complete

--- cart_api/routes/cartitems.py
+++ cart_api/routes/cartitems.py
@@ -12,27 +12,39 @@ from cart_api.database import DatabaseCartItem
 
 class CartItems:
     def on_post(self, req, resp):
-        new_product = req.get_media()
+        new_cartitem = req.get_media()
         new_model = DatabaseCartItem(
-            name=new_product["name"],
-            price=new_product["price"],
-            quantity=new_product["quantity"],
-            product_id=new_product["product_id"],
+            name=new_cartitem["name"],
+            price=new_cartitem["price"],
+            quantity=new_cartitem["quantity"],
+            product_id=new_cartitem["product_id"],
         )
         new_model.save()
         resp.media = model_to_dict(new_model)
         resp.status = falcon.HTTP_201
 
     def on_get(self, req, resp):
-        pass
+        resp.media = []
+        cartitems = DatabaseCartItem.select()
+        for cartitem in cartitems:
+            resp.media.append(model_to_dict(cartitem))
+        resp.status = falcon.HTTP_200
 
 
 class CartItem:
-    def on_get(self, req, resp):
-        pass
+    def on_get(self, req, resp, cartitem_id):
+        cartitem = DatabaseCartItem.get(id=cartitem_id)
+        resp.media = model_to_dict(cartitem)
+        resp.status = falcon.HTTP_200
 
-    def on_delete(self, req, resp):
-        pass
+    def on_delete(self, req, resp, cartitem_id):
+        DatabaseCartItem.delete_by_id(cartitem_id)
+        resp.status = falcon.HTTP_204
 
-    def on_patch(self, req, resp):
-        pass
+    def on_patch(self, req, resp, cartitem_id):
+        cartitem = DatabaseCartItem.get(id=cartitem_id)
+        changes = req.media
+        if "quantity" in changes:
+            cartitem.quantity = changes["quantity"]
+            cartitem.save()
+        resp.status = falcon.HTTP_204
